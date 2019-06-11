da0bf3b5ca196fe72103e4506d794b54547f1418
Kevin Carmona-Murphy <kcmurphy@L-KCMURPHY-TECH.local>
2019-06-11 17:08:26 -0400

    add lorem picsum images, footer, and a few extra migrations man

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_143242) do
+ActiveRecord::Schema.define(version: 2019_06_11_210204) do
 
   create_table "todos", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
